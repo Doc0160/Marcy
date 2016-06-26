b5da11cd43250370ba96b507b1a25e2ce0ced0ef
Doc0160 <t0160d@gmail.com>
2016-06-27 00:11:29 +0200

    build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,10 @@
 @echo off
+set CommonCompilerFlags=-DMARCY_DEBUG=1 -DMARCY_SLOW=1 -WX -W4 -wd4201 -wd4100 -wd4189 -MT -Gm- -GR- -EHa- -Od -Oi -FC -Z7 -nologo -Fmwin32_marcy.map
+set CommonLinkererFlags=-opt:ref kernel32.lib user32.lib Gdi32.lib
 ibt.exe -begin marcy.ibt
 pushd .\bin
-cl -DMARCY_DEBUG=1 -DMARCY_SLOW=1 -WX -W4 -wd4201 -wd4100 -wd4189 -MT -Gm- -GR- -EHa- -Od -Oi -FC -Z7 -nologo -Fmwin32_marcy.map ..\code\win32_platform.cpp /link -subsystem:windows,5.02 -opt:ref kernel32.lib user32.lib Gdi32.lib 
+cl %CommonCompilerFlags% ..\code\win32_platform.cpp /link -subsystem:windows,5.2 %CommonLinkererFlags%
+REM cl %CommonCompilerFlags% ..\code\win32_platform.cpp /link -subsystem:windows,5.1 %CommonLinkererFlags%
 set LastError=%ERRORLEVEL%
 popd
 ibt.exe -end marcy.ibt %LastError%
